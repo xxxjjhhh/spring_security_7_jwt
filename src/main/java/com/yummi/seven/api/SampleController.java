00f5a7915675d132324205a408c023952b1a2ae2
kimjihun <xxxjjhhh@naver.com>
2026-05-29 08:55:34 +0900

    * 2강

---
+++ src/main/java/com/yummi/seven/api/SampleController.java
@@ -0,0 +1,26 @@
+package com.yummi.seven.api;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/api/v1")
+public class SampleController {
+
+    @GetMapping("/")
+    public String index() {
+        return "index";
+    }
+
+    @GetMapping("/user")
+    public String user() {
+        return "user";
+    }
+
+    @GetMapping("/admin")
+    public String admin() {
+        return "admin";
+    }
+
+}
---
+++ src/test/java/com/yummi/seven/SevenApplicationTests.java
@@ -0,0 +1,13 @@
+package com.yummi.seven;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SevenApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
